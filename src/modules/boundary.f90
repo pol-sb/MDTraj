187584e8bcbb90a86289325d1a66ab96f03c5e40
Lucas Fernandez Stolpa <lucas.fernandez.stolpa@gmail.com>
2022-03-02 18:04:54 +0100

    Initial code of boundary periodic conditions

--- src/modules/boundary.f90
+++ src/modules/boundary.f90
@@ -1,2 +1,23 @@
+module boundary
+!   use params
+   implicit none
+   integer :: ii, jj, kk, M
 
-	
+   contains
+
+		 !===========================================================================!
+	   !               				PERIODIC BOUNDARY CONDITIONS
+	   !===========================================================================!
+	   ! Periodic boundary conditions function
+	   subroutine pbc(x,boxlength,origin)
+			 implicit none
+	     double precision, intent(in) :: boxlength
+	     double precision, intent(inout) :: x
+	     if (x.gt.(boxlength/2.d0 + origin)) then
+	       x = x - boxlength
+	     elseif (x.lt.-(boxlength/2.d0 - origin)) then
+	       x = x + boxlength
+	     end if
+	   end subroutine pbc
+
+end module boundary
