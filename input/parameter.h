ed1fe46c6c3bfcf6e92f06963aad20e39e5b15a2
Lucas Fernandez Stolpa <lucas.fernandez.stolpa@gmail.com>
2022-03-05 10:36:13 +0100

    Inclusion de LJ parameters

--- input/parameter.h
+++ input/parameter.h
@@ -5,18 +5,21 @@
 	!double precision::density=2.5
 	double precision::density=10.0d0 !Lomgitude of the size box
 	integer::dimension=3 !Dimension of the system.
-	
-	integer::structure=1 !For SC:1, for fcc:2, for diamond 3.  
-	
+
+	integer::structure=1 !For SC:1, for fcc:2, for diamond 3.
+
 	double precision::temp= 300.0d0  !Temperature (K)
 	integer::vel_opt=1 ! if 1, implemented bimodal. Else, set up to 0
-	
+
 	!Simulation set up
 	!-----------------
 	integer::ntimes=6 !Steps of the simulation
 	double precision:: dt= 0.005 !Time step (ps)
-	
-	
+
 	!Force parameters
 	!----------------
 	double precision::rc= 0.05d0 !Cut-off
+
+	!Lennard-Jones parameters
+	!----------------
+	double precision:: epsilon = 10.22, sigma = 2.556
