041fc00f7383b94591941cfd14fcb9ccd43c94b5
Mtunica <mtunicro18@alumnes.ub.edu>
2022-03-05 10:22:41 +0100

    change to density and particles per side

--- input/parameter.h
+++ input/parameter.h
@@ -1,22 +1,22 @@
 
 	!	Initialization parameters
 	!---------------------------
-	integer::natoms=32 !Number of particles
-	!double precision::density
-	double precision::L=10.0d0 !Lomgitude of the size box
+	integer::nc=2 !Number of particles per side
+	!double precision::density=2.5
+	double precision::density=10.0d0 !Lomgitude of the size box
 	integer::dimension=3 !Dimension of the system.
 	
-	integer::structure=2 !For SC:1, for fcc:2, for diamond 3.  
+	integer::structure=1 !For SC:1, for fcc:2, for diamond 3.  
 	
 	double precision::temp= 300.0d0  !Temperature (K)
 	integer::vel_opt=1 ! if 1, implemented bimodal. Else, set up to 0
 	
 	!Simulation set up
 	!-----------------
-	integer::ntimes=100 !Steps of the simulation
+	integer::ntimes=6 !Steps of the simulation
 	double precision:: dt= 0.005 !Time step (ps)
 	
 	
 	!Force parameters
 	!----------------
-	double precision::rc= 5.0d0 !Cut-off
+	double precision::rc= 0.05d0 !Cut-off
