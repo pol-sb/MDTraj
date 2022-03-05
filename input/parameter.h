0e288e09fa529514913048b11462de359f43336c
Lucas Fernandez Stolpa <lucas.fernandez.stolpa@gmail.com>
2022-03-05 12:57:59 +0100

    05-03-2022 code review input

--- input/parameter.h
+++ input/parameter.h
@@ -1,9 +1,8 @@
 
 	!	Initialization parameters
 	!---------------------------
-	integer::nc=2 !Number of particles per side
-	!double precision::density=2.5
-	double precision::density=10.0d0 !Lomgitude of the size box
+	integer::nc=4 !Number of particles per side
+	double precision::density=0.4d0 !Lomgitude of the size box
 	integer::dimension=3 !Dimension of the system.
 
 	integer::structure=1 !For SC:1, for fcc:2, for diamond 3.
@@ -13,7 +12,8 @@
 
 	!Simulation set up
 	!-----------------
-	integer::ntimes=6 !Steps of the simulation
+	integer::ntimes=100 !Steps of the simulation
+	integer::everyt=10
 	double precision:: dt= 0.005 !Time step (ps)
 
 	!Force parameters
