75ec654016b0aa87caf965d92bfa95508c4ed5c6
Mtunica <mtunicro18@alumnes.ub.edu>
2022-03-05 01:47:58 +0100

    modification of parameter.h, modification of main, modification of makefile, modification of boundary, modification of forces, modification of initialization, modification of integrators, modification of thermostat. Debug. Add constants.h

--- src/main.f90
+++ src/main.f90
@@ -1,26 +1,33 @@
 program main
 
 use initialization
+use boundary
+use integrators
 
 implicit none
 include "../input/parameter.h"
-     
-double precision,allocatable::r(:,:),v(:,:)
+integer::tt,gg
+double precision::ti
+double precision,allocatable::r(:,:),v(:,:),F(:,:)
+double precision::ngr, pressp
+double precision::epot,deltag
+double precision, allocatable, dimension(:) :: r2t, gr
+integer::nhis
 
-	allocate(r(N,3),v(N,3))
+	nhis = 250; deltag = L/(2.d0*dble(nhis))
+   allocate(gr(nhis)); gr = 0.d0
+
+	allocate(r(natoms,3),v(natoms,3),F(natoms,3))
       
-	if (structure .eq. 0) then
-		!read from file
-		
-		
-	elseif (structure .eq. 1) then
-		! Call SC
+	!Initialization of the structure
+	if (structure .eq. 1) then
+		call sc(natoms,L,r)
 	
 	elseif (structure .eq. 2) then
-		! Call FCC
+		call fcc(natoms,L,r)
 		
 	elseif (structure .eq. 3) then
-		call diamond(L, N, r)
+		call diamond( natoms,L,r)
 
 	else 
 		write(*,*)"Error, no structure found"
@@ -28,8 +35,24 @@ double precision,allocatable::r(:,:),v(:,:)
 		
 	endif
 	
+	!initialization of velocity
+	if (vel_opt .eq. 1) then
+		call bimodal(Temp,v)
+	else
+		v(:,:)=0.0d0
+	endif
 	
-	
+	call force(natoms,r,L,rc,F,epot,pressp,gr,deltag)
+	do tt = 1,ntimes,1
+		ti = ti+dt ! Actualizing the instant time
+     	call vel_verlet_with_thermo(natoms,r,v,F,epot,dt,rc,L,Temp,pressp,gr,deltag)
+     	ngr = ngr+1
+     	
+     	if (gg.lt.1e4) then
+       		gr = 0.d0
+     	end if
+     	
+   end do
 	
 	
 	
--- src/modules/thermostats.f90
+++ src/modules/thermostats.f90
@@ -1,2 +1,62 @@
+module thermostat
+	 implicit none
+   contains
 
-	
+   !===========================================================================!
+   !                              ANDERSEN THERMOSTAT
+   !===========================================================================!
+   subroutine andersen_thermo(Temp,vel,natoms)
+   include "constants.h"
+   double precision, dimension(:,:) :: vel
+   integer,intent(in)::natoms
+   double precision :: nu, Temp, sigma
+   double precision, dimension(size(vel,2)) :: x_rand
+   double precision, dimension(4) :: vel_normalrand
+
+   nu = 1e-3
+   sigma = dsqrt(Temp)
+   call random_number(x_rand)
+
+   do ii = 1,natoms
+     if (x_rand(ii).lt.nu) then ! choosing if velocity of particle i gets changed
+       call normal_rand(sigma,vel_normalrand(1),vel_normalrand(2))
+       call normal_rand(sigma,vel_normalrand(3),vel_normalrand(4))
+       ! The subroutine normal_rand returns a random number from a normal
+       ! distribution with standard deviation \sigma = sqrt(T)
+       do jj = 1,3
+         vel(jj,ii) = vel_normalrand(jj)
+       end do
+     end if
+   end do
+
+   end subroutine
+
+   !===========================================================================!
+   !                        NORMAL RANDOM NUMBER GENERATOR
+   !===========================================================================!
+   subroutine normal_rand(sigma, xout1, xout2)
+   include "constants.h"
+   double precision :: sigma
+   double precision xout1, xout2
+   double precision :: x(2)
+
+   call random_number(x)
+
+   xout1 = sigma*dsqrt(-2d0*(dlog(1d0-x(1))))*dcos(2d0*PI*x(2))
+   xout2 = sigma*dsqrt(-2d0*(dlog(1d0-x(1))))*dsin(2d0*PI*x(2))
+
+   end subroutine normal_rand
+
+
+   !===========================================================================!
+   !                        NORMAL RANDOM NUMBER GENERATOR
+   !===========================================================================!
+   double precision function kinetic(vel,natoms) result(ekin)
+   include "constants.h"
+   integer,intent(in)::natoms
+   double precision, allocatable, dimension(:,:), intent(in) :: vel
+     do ii = 1,natoms
+       ekin = ekin + 0.5d0*(vel(1,ii)**2 + vel(2,ii)**2 + vel(3,ii)**2)
+     end do
+   end function kinetic
+endmodule thermostat
