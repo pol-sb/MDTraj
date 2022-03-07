e269f23304ca6a48bee515bdbbc22f57570d09c0
Mtunica <mtunicro18@alumnes.ub.edu>
2022-03-07 15:59:50 +0100

    Delete non-used variables in parameter.h and correction of makefile

--- input/parameter.h
+++ input/parameter.h
@@ -1,20 +1,19 @@
 
 	!	Initialization parameters
 	!---------------------------
-	integer::nc=4 !Number of particles per side
-	double precision::density=0.4d0 !Lomgitude of the size box
-	integer::dimension=3 !Dimension of the system.
+	integer::nc=6 !Number of particles per side
+	double precision::density=0.005d0 !Lomgitude of the size box
 
-	integer::structure=1 !For SC:1, for fcc:2, for diamond 3.
+	integer::structure=3 !For SC:1, for fcc:2, for diamond 3.
 
 	double precision::temp= 300.0d0  !Temperature (K)
 	integer::vel_opt=1 ! if 1, implemented bimodal. Else, set up to 0
 
 	!Simulation set up
 	!-----------------
-	integer::ntimes=100 !Steps of the simulation
+	integer::ntimes=50000 !Steps of the simulation
 	integer::everyt=10
-	double precision:: dt= 0.005 !Time step (ps)
+	double precision:: dt= 0.0001 !Time step (ps)
 
 	integer::thermo=1 !For no thermostat:0, for active thermostat:1
 
--- makefile
+++ makefile
@@ -1,24 +1,34 @@
 PY_DIST != which python3
 
-all:src/program.exe
+## By default, it is all (just write make)
+
+## 1. make all - to compile, execute and plot.
+all:
 	$(MAKE) -C src/
 	./src/program.exe
 	${PY_DIST} ./src/modules/plot_statistics.py
 
+## 2. make compile - to compile.
 compile:
 	$(MAKE) -C src/
 	
+## 3. make run - to run.
 run:
 	 ./src/program.exe
 
-src/program.exe:
-	$(MAKE) -C src/
-
-clear:
-	rm -r src/build
-	rm -r src/program.exe
-
+## 4. make plot - to plot the graphics
 plot:
 	@echo "Python found in: ${PY_DIST} "
 	@${PY_DIST} ./src/modules/plot_statistics.py
-	
+
+## 5. make clean - to remove build and program.exe
+clean:
+	rm -r src/build
+	rm -r src/program.exe
+
+
+## ADVERTISING: If you change the parameters, you need to recompile. 
+
+.PHONY : help
+help:
+	@sed -n 's/^##//p' makefile
