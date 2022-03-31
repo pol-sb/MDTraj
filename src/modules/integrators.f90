e6207d7fff478977471405f2b4dbfd3fd3f334f2
Lucas Fernandez Stolpa <lucas.fernandez.stolpa@gmail.com>
2022-03-31 16:49:31 +0200

    Actualized for paralelization does not work at the moment

--- src/modules/integrators.f90
+++ src/modules/integrators.f90
@@ -3,12 +3,12 @@
 ! This module includes all the principal integration algorithms used in the
 ! molecular dynamics code, as the principal algorithm and to check that it
 ! works properly.
-! 
+!
 ! This module needs the parameters module (params.f90) and tools (tools.f90)
 ! in order to work properly
 !=====================================================================================!
 ! The module contains:
-!       -> euler (natoms,r,vel,F,dt,boxlength): 
+!       -> euler (natoms,r,vel,F,dt,boxlength):
 !           Euler Integration Method. It returns the new positions and velocities.
 !                   Input:
 !                     - natoms (número de átomos)(in) : integer scalar
@@ -19,10 +19,10 @@
 !                   Output:
 ! 					   - r (positions)(inout) : double precision array
 !                      - vel (velocity)(inout) : double precision array
-! 
-! 
-!       -> vel_verlet (natoms,r,vel,F,Upot,dt,rc,boxlength,pressp,gr,deltag): 
-!           The verlet algorithm method wihout considering the temperature. 
+!
+!
+!       -> vel_verlet (natoms,r,vel,F,Upot,dt,rc,boxlength,pressp,gr,deltag):
+!           The verlet algorithm method wihout considering the temperature.
 !			It returns the new positions and velocities.
 !                   Input:
 !                      - natoms (número de átomos)(in) : integer scalar
@@ -41,10 +41,10 @@
 !	   				    - Pressp (pressure) (out) : double precision scalar
 !						  Depencency:
 !      					  - force() : In module forces (src/modules/forces.f90)
-! 
-! 
-!       -> vel_verlet_with_thermo (natoms,r,vel,F,Upot,dt,rc,boxlength,Temp,pressp,gr,deltag): 
-!           The verlet algorithm method considering the temperature. 
+!
+!
+!       -> vel_verlet_with_thermo (natoms,r,vel,F,Upot,dt,rc,boxlength,Temp,pressp,gr,deltag):
+!           The verlet algorithm method considering the temperature.
 !			It returns the new positions and velocities.
 !                   Input:
 !                      - natoms (número de átomos)(in) : integer scalar
@@ -64,13 +64,13 @@
 !	   				   - Pressp (pressure) (out) : double precision scalar
 !						  Depencency:
 !      					  - force() : In module forces (src/modules/forces.f90)
-! 							  - andersen_thermo() : In module thermostat (src/modules/thermostats.f90) 
+! 							  - andersen_thermo() : In module thermostat (src/modules/thermostats.f90)
 !=====================================================================================!
 module integrators
   	use forces
 	use thermostat
 	implicit none
-	  
+
    contains
 
 !=====================================================================================!
@@ -91,7 +91,8 @@ module integrators
    	double precision, allocatable, intent(in) :: F(:,:)
    	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
    	double precision, intent(in) :: dt, boxlength
-   
+    integer :: ii, jj
+
   		do jj = 1,natoms
      		do ii = 1,3
        		r(jj,ii) = r(jj,ii) + vel(jj,ii)*dt + 0.5d0*F(jj,ii)*dt*dt
@@ -100,7 +101,7 @@ module integrators
    	enddo
 
    endsubroutine euler
-   
+
 !=====================================================================================!
 !                     VELOCITY VERLET INTEGRATION (Without temperature)
 !=====================================================================================!
@@ -122,30 +123,33 @@ module integrators
 !  Depencency:
 !      - force() : In module forces (src/modules/forces.f90)
 !=====================================================================================!
-   subroutine vel_verlet(natoms,r,vel,F,Upot,dt,rc,boxlength,pressp,gr,deltag)
-		integer,intent(in)::natoms
+   subroutine vel_verlet(natoms,r,vel,F,Upot,dt,rc,boxlength,pressp,gr,deltag,&
+     particle_range,interact_range)
+		integer,intent(in)::natoms, particle_range(2), interact_range(2)
      	double precision, allocatable,intent(inout) :: F(:,:)
      	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
      	double precision, allocatable, intent(inout) :: gr(:)
      	double precision, intent(in) :: dt, rc, boxlength, deltag
      	double precision, intent(out) :: Upot, pressp
-      	 
+      integer :: ii, jj
 
-		do ii = 1,natoms
-		 	do jj = 1,3
+
+		do jj = particle_range(1),particle_range(2)
+		 	do ii = 1,3
 				r(jj,ii) = r(jj,ii) + vel(jj,ii)*dt + 0.5d0*F(jj,ii)*dt*dt
 				vel(jj,ii) = vel(jj,ii) + F(jj,ii)*0.5d0*dt
 			enddo
 		enddo
 
-		call force(natoms,r,boxlength,rc,F,Upot,pressp,gr,deltag)
-		
-		do ii = 1,natoms
-			do jj = 1,3
+		call force(natoms,r,boxlength,rc,F,Upot,pressp,gr,deltag,interact_range,&
+              interact_list)
+
+		do jj = particle_range(1),particle_range(2)
+			do ii = 1,3
 				vel(jj,ii) = vel(jj,ii) + F(jj,ii)*0.5d0*dt
 			enddo
 		enddo
-		
+
    endsubroutine vel_verlet
 
 !=====================================================================================!
@@ -169,36 +173,49 @@ module integrators
 !	   - Pressp (pressure) (out) : double precision scalar
 !  Depencency:
 !      - force() : In module forces (src/modules/forces.f90)
-!      - andersen_thermo() : In module thermostat (src/modules/thermostats.f90) 
+!      - andersen_thermo() : In module thermostat (src/modules/thermostats.f90)
 !=====================================================================================!
-	subroutine vel_verlet_with_thermo(natoms,r,vel,F,Upot,dt,rc,boxlength,Temp,pressp,gr,deltag)
-		integer,intent(in)::natoms
-     	double precision, allocatable,  intent(inout) :: F(:,:)
-     	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
-     	double precision, allocatable,  intent(inout) :: gr(:)
-     	double precision, intent(in) :: dt, rc, boxlength, Temp, deltag
-     	double precision, intent(out) :: Upot, pressp
-		 
+	subroutine vel_verlet_with_thermo(natoms,r,vel,F,Upot,dt,rc,boxlength,Temp,pressp,&
+    gr,deltag,particle_range,interact_range,interact_list)
+		integer,intent(in)::natoms, particle_range(2), interact_range(2)
+    integer, allocatable, intent(in)::interact_list(:,:)
+   	double precision, allocatable,  intent(inout) :: F(:,:)
+   	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
+   	double precision, allocatable,  intent(inout) :: gr(:)
+   	double precision, intent(in) :: dt, rc, boxlength, Temp, deltag
+   	double precision, intent(out) :: Upot, pressp
+    integer ii, jj
+
 		Upot = 0.d0; pressp = 0.d0
 
-		do jj = 1,natoms
+		do jj = particle_range(1),particle_range(2)
 			do ii = 1,3
          	r(jj,ii) = r(jj,ii) + vel(jj,ii)*dt + 0.5d0*F(jj,ii)*dt*dt
 				vel(jj,ii) = vel(jj,ii) + F(jj,ii)*0.5d0*dt
 			enddo
 		enddo
+    ! allgather should be applied into the the r and vel array
 
-		call force(natoms,r,boxlength,rc,F,Upot,pressp,gr,deltag)
+		call force(natoms,r,boxlength,rc,F,Upot,pressp,gr,deltag,interact_range,&
+              interact_list)
 
-		do jj = 1,natoms
+    call MPI_Reduce(sendbuf, recvbuf, count, datatype, op, root, comm, ierror)
+    ! reduce should be applied into Upot,pressp, and gr
+
+    call MPI_ALLGATHER(SENDBUF, SENDCOUNT, SENDTYPE, RECVBUF, RECVCOUNT,
+                  RECVTYPE, COMM, IERROR)
+    ! allgather should be applied into the the F array
+
+		do jj = particle_range(1),particle_range(2)
 			do ii = 1,3
-         	vel(jj,ii) = vel(jj,ii) + F(jj,ii)*0.5d0*dt
-       	enddo
-     	enddo
+        vel(jj,ii) = vel(jj,ii) + F(jj,ii)*0.5d0*dt
+      enddo
+    enddo
+    ! allgather should be applied into the the r and vel array
 
-     	call andersen_thermo(Temp,vel,natoms)
-     	
+    call andersen_thermo(Temp,vel,natoms,particle_range)
+    ! allgather should be applied into the the vel array
+
    endsubroutine vel_verlet_with_thermo
 
 endmodule integrators
-	
