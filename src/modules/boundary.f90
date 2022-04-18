bbfbf3cf740eb982f7809e4d6a6457f2ef4beb57
Pol Sanz <me@polsanz.xyz>
2022-04-19 00:49:54 +0200

    Additional formmating, fixed python, indentation modification, added .gitignore and makefile

--- src/modules/boundary.f90
+++ src/modules/boundary.f90
@@ -1,50 +1,49 @@
 !==============================================================================!
 !                          MODULE BOUNDARY
 !==============================================================================!
-! Module that contains the procces to create periodic boundary conditions in 
+! Module that contains the procces to create periodic boundary conditions in
 ! a 3D squared system.
 !==============================================================================!
 ! The module contains:
-!       -> pbc(): 
-!				Process that controls the periodic boundary conditions
+!       -> pbc():
+!          Process that controls the periodic boundary conditions
 !                   Input:
 !                       - x (distance)(inout) : double precision scalar
-!								- boxlength (in) : double precision scalar
+!                       - boxlength (in) : double precision scalar
 !                       - origin (in) : double precision scalar
 !                   Output:
 !                       - x (distance)(inout) : double precision scalar
 !==============================================================================!
 module boundary
-	implicit none
- 
-contains   
+    implicit none
+
+contains
 !===========================================================================!
-!               				PERIODIC BOUNDARY CONDITIONS
+!                    PERIODIC BOUNDARY CONDITIONS
 !===========================================================================!
 ! Periodic boundary conditions function for forces.
 !===========================================================================!
 ! Input:
-!		- x (distance)(inout) : double precision scalar
-!		- boxlength (in) : double precision scalar
-!		- origin (in) : double precision scalar
-!					Origin of the cell of simulation.
+!                - x (distance)(inout) : double precision scalar
+!                - boxlength (in) : double precision scalar
+!                - origin (in) : double precision scalar
+!                                        Origin of the cell of simulation.
 ! Output:
-!		- origin (in) : double precision scalar
-!					Origin of the cell of simulation.
+!                - origin (in) : double precision scalar
+!                                        Origin of the cell of simulation.
 !===========================================================================!
-	subroutine pbc(x,boxlength,origin)
-		double precision, intent(in) :: boxlength,origin
-		double precision, intent(inout) :: x
+    subroutine pbc(x, boxlength, origin)
+        double precision, intent(in) :: boxlength, origin
+        double precision, intent(inout) :: x
 
-		if (x.gt.(origin + boxlength/2.d0)) then
-			x = x - boxlength
-			
-		elseif (x.lt.(origin - boxlength/2.d0)) then
-			x = x + boxlength
-			
-		endif
-		
-	endsubroutine pbc
+        if (x .gt. (origin + boxlength/2.d0)) then
+            x = x - boxlength
 
+        elseif (x .lt. (origin - boxlength/2.d0)) then
+            x = x + boxlength
 
-endmodule boundary
+        end if
+
+    end subroutine pbc
+
+end module boundary
--- src/modules/integrators.f90
+++ src/modules/integrators.f90
@@ -1,7 +1,7 @@
 !=====================================================================================!
 !                               MODULE INTEGRATORS
 ! This module includes all the principal integration algorithms used in the
-! molecular dynamics code, as the principal algorithm and to check that it
+! molecular dynamics code,as the principal algorithm and to check that it
 ! works properly.
 !
 ! This module needs the parameters module (params.f90) and tools (tools.f90)
@@ -10,209 +10,208 @@
 ! The module contains:
 !       -> euler (natoms,r,vel,F,dt,boxlength):
 !           Euler Integration Method. It returns the new positions and velocities.
-!                   Input:
+!             Input:
 !                     - natoms (número de átomos)(in) : integer scalar
-!	   				  - r (positions)(inout) : double precision array
-!      				  - F (force)(inout) : double precision array
-! 	   				  -dt (time step)(in) : double precision scalar
-!	   				  -boxlength (longitude of one side)(in) double precision scalar
-!                   Output:
-! 					   - r (positions)(inout) : double precision array
-!                      - vel (velocity)(inout) : double precision array
-!
+!                     - r (positions)(inout) : double precision array
+!                     - F (force)(inout) : double precision array
+!                     - dt (time step)(in) : double precision scalar
+!                     - boxlength (longitude of one side)(in) double precision scalar
+!             Output:
+!                     - r (positions)(inout) : double precision array
+!                     - vel (velocity)(inout) : double precision array
 !
 !       -> vel_verlet (natoms,r,vel,F,Upot,dt,rc,boxlength,pressp,gr,deltag):
 !           The verlet algorithm method wihout considering the temperature.
-!			It returns the new positions and velocities.
-!                   Input:
-!                      - natoms (número de átomos)(in) : integer scalar
-! 	   				   - r (positions)(inout) : double precision array
-!      				   - vel (velocity)(inout) : double precision array
-!      				   - F (force)(inout) : double precision array
-! 	   				   -dt (time step)(in) : double precision scalar
-!	   				   -rc (cut-off distance) (in) : double precision
-!	   				   -boxlength (longitude of one side)(in) : double precision scalar
-!	   				   -gr ()(in) : double precision array
-!      				   -deltag ()(in) : double precision scalar
-!                   Output:
+!           It returns the new positions and velocities.
+!             Input:
+!                     - natoms (número de átomos)(in) : integer scalar
 !                     - r (positions)(inout) : double precision array
-!      				    - vel (velocity)(inout) : double precision array
-!	   				    - Upot (potential energy) (out) : double precision scalar
-!	   				    - Pressp (pressure) (out) : double precision scalar
-!						  Depencency:
-!      					  - force() : In module forces (src/modules/forces.f90)
+!                     - vel (velocity)(inout) : double precision array
+!                     - F (force)(inout) : double precision array
+!                     - dt (time step)(in) : double precision scalar
+!                     - rc (cut-off distance) (in) : double precision
+!                     - boxlength (longitude of one side)(in) : double precision scalar
+!                     - gr ()(in) : double precision array
+!                     - deltag ()(in) : double precision scalar
+!             Output:
+!                     - r (positions)(inout) : double precision array
+!                     - vel (velocity)(inout) : double precision array
+!                     - Upot (potential energy) (out) : double precision scalar
+!                     - Pressp (pressure) (out) : double precision scalar
+! Dependencies:
+!      - force() : In module forces (src/modules/forces.f90)
 !
 !
 !       -> vel_verlet_with_thermo (natoms,r,vel,F,Upot,dt,rc,boxlength,Temp,pressp,gr,deltag):
 !           The verlet algorithm method considering the temperature.
-!			It returns the new positions and velocities.
-!                   Input:
-!                      - natoms (número de átomos)(in) : integer scalar
-! 	   				   - r (positions)(inout) : double precision array
-!      				   - vel (velocity)(inout) : double precision array
-!      				   - F (force)(inout) : double precision array
-! 	   				   -dt (time step)(in) : double precision scalar
-!	   				   -rc (cut-off distance) (in) : double precision
-!	   				   -boxlength (longitude of one side)(in) : double precision scalar
-!	   				   -gr ()(in) : double precision array
-!      				   -deltag ()(in) : double precision scalar
-!                   Output:
-!                      - r (positions)(inout) : double precision array
-!      				   - vel (velocity)(inout) : double precision array
-!	   				   - Upot (potential energy)(out) : double precision scalar
-! 	   				   - temp (temperature)(in) : double precision scalar
-!	   				   - Pressp (pressure) (out) : double precision scalar
-!						  Depencency:
-!      					  - force() : In module forces (src/modules/forces.f90)
-! 							  - andersen_thermo() : In module thermostat (src/modules/thermostats.f90)
+!           It returns the new positions and velocities.
+!             Input:
+!                     - natoms (número de átomos)(in) : integer scalar
+!                     - r (positions)(inout) : double precision array
+!                     - vel (velocity)(inout) : double precision array
+!                     - F (force)(inout) : double precision array
+!                     - dt (time step)(in) : double precision scalar
+!                     - rc (cut-off distance) (in) : double precision
+!                     - boxlength (longitude of one side)(in) : double precision scalar
+!                     - gr ()(in) : double precision array
+!                     - deltag ()(in) : double precision scalar
+!             Output:
+!                     - r (positions)(inout) : double precision array
+!                     - vel (velocity)(inout) : double precision array
+!                     - Upot (potential energy)(out) : double precision scalar
+!                     - temp (temperature)(in) : double precision scalar
+!                     - Pressp (pressure) (out) : double precision scalar
+! Dependencies:
+!      - force() : In module forces (src/modules/forces.f90)
+!      - andersen_thermo() : In module thermostat (src/modules/thermostats.f90)
 !=====================================================================================!
 module integrators
-  use forces
-	use thermostat
-  use mpi
-	implicit none
+    use forces
+    use thermostat
+    use mpi
+    implicit none
 
-  contains
+contains
 
 !=====================================================================================!
 !                     VELOCITY VERLET INTEGRATION (Without temperature)
 !=====================================================================================!
 !  Input:
 !      - natoms (número de átomos)(in) : integer scalar
-! 	   - r (positions)(inout) : double precision array
+!      - r (positions)(inout) : double precision array
 !      - vel (velocity)(inout) : double precision array
 !      - F (force)(inout) : double precision array
-! 	   -dt (time step)(in) : double precision scalar
-!	   -rc (cut-off distance) (in) : double precision
-!	   -boxlength (longitude of one side)(in) : double precision scalar
-!	   -gr ()(in) : double precision array
-!      -deltag ()(in) : double precision scalar
+!      - dt (time step)(in) : double precision scalar
+!      - rc (cut-off distance) (in) : double precision
+!      - boxlength (longitude of one side)(in) : double precision scalar
+!      - gr ()(in) : double precision array
+!      - deltag ()(in) : double precision scalar
 !  Output:
-!	   - r (positions)(inout) : double precision array
+!      - r (positions)(inout) : double precision array
 !      - vel (velocity)(inout) : double precision array
-!	   - Upot (potential energy) (out) : double precision scalar
-!	   - Pressp (pressure) (out) : double precision scalar
+!      - upot (potential energy) (out) : double precision scalar
+!      - pressp (pressure) (out) : double precision scalar
 !  Depencency:
 !      - force() : In module forces (src/modules/forces.f90)
 !=====================================================================================!
-   subroutine vel_verlet(natoms,r,vel,F,epot,dt,rc,boxlength,pressp,&
-			 gr,deltag,particle_range,sizes,displs,taskid)
-	include "../declaration_variables/parallel_variables.h"
-		integer,intent(in)::natoms, particle_range(2)
-    		integer, allocatable, intent(in):: sizes(:), displs(:)
-	   	double precision, allocatable,  intent(inout) :: F(:,:)
-	   	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
-	   	double precision, allocatable,  intent(inout) :: gr(:)
-	   	double precision, intent(in) :: dt, rc, boxlength, deltag
-	   	double precision, intent(out) :: pressp, epot
-    		integer ii, jj, jv
+    subroutine vel_verlet(natoms, r, vel, F, epot, dt, rc, boxlength, pressp, &
+                          gr, deltag, particle_range, sizes, displs, taskid)
+        include "../declaration_variables/parallel_variables.h"
+        integer, intent(in)::natoms, particle_range(2)
+        integer, allocatable, intent(in):: sizes(:), displs(:)
+        double precision, allocatable, intent(inout) :: F(:, :)
+        double precision, allocatable, intent(inout) :: r(:, :), vel(:, :)
+        double precision, allocatable, intent(inout) :: gr(:)
+        double precision, intent(in) :: dt, rc, boxlength, deltag
+        double precision, intent(out) :: pressp, epot
+        integer ii, jj, jv
 
-	    	first_particle = particle_range(1); last_particle = particle_range(2)
-	    	call MPI_BARRIER(MPI_COMM_WORLD, ierror)
-	    ! <------ aqui se necesitan las fuerzas repartidas entre todos los workers
-			do jj = particle_range(1),particle_range(2)
-	      			jv = jj - particle_range(1) + 1
-				do ii = 1,3
-					r(jj,ii) = r(jj,ii) + vel(jv,ii)*dt + 0.5d0*F(jv,ii)*dt*dt
-					vel(jv,ii) = vel(jv,ii) + F(jv,ii)*0.5d0*dt
-				enddo
-			enddo
-	    	call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        first_particle = particle_range(1); last_particle = particle_range(2)
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        ! <------ aqui se necesitan las fuerzas repartidas entre todos los workers
+        do jj = particle_range(1), particle_range(2)
+            jv = jj - particle_range(1) + 1
+            do ii = 1, 3
+                r(jj, ii) = r(jj, ii) + vel(jv, ii)*dt + 0.5d0*F(jv, ii)*dt*dt
+                vel(jv, ii) = vel(jv, ii) + F(jv, ii)*0.5d0*dt
+            end do
+        end do
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
 
-		    do ii = 1,3
-		      call MPI_allgatherv(r(first_particle:last_particle,ii),(last_particle-first_particle+1),&
-				  MPI_DOUBLE_PRECISION,r(:,ii),sizes,displs,MPI_DOUBLE_PRECISION,&
-				  MPI_COMM_WORLD,ierror)
-		    end do
-		    call MPI_BARRIER(MPI_COMM_WORLD, ierror)
-		    ! allgather should be applied into the the r and vel array
-		    ! <------- aqui se necesita haber repartido todas las posiciones
-				call force(natoms,r,boxlength,rc,F,particle_range)
+        do ii = 1, 3
+            call MPI_allgatherv(r(first_particle:last_particle, ii), (last_particle - first_particle + 1), &
+                                MPI_DOUBLE_PRECISION, r(:, ii), sizes, displs, MPI_DOUBLE_PRECISION, &
+                                MPI_COMM_WORLD, ierror)
+        end do
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        ! allgather should be applied into the the r and vel array
+        ! <------- aqui se necesita haber repartido todas las posiciones
+        call force(natoms, r, boxlength, rc, F, particle_range)
 
-		    call potential(natoms,r,boxlength,rc,epot,pressp,gr,deltag,particle_range,taskid)
+        call potential(natoms, r, boxlength, rc, epot, pressp, gr, deltag, particle_range, taskid)
 
-		    call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
 
-				do jj = particle_range(1),particle_range(2)
-		      jv = jj - particle_range(1) + 1
-					do ii = 1,3
-			vel(jv,ii) = vel(jv,ii) + F(jv,ii)*0.5d0*dt
-		      enddo
-		    enddo
-		    ! allgather should be applied into the the r and vel array
+        do jj = particle_range(1), particle_range(2)
+            jv = jj - particle_range(1) + 1
+            do ii = 1, 3
+                vel(jv, ii) = vel(jv, ii) + F(jv, ii)*0.5d0*dt
+            end do
+        end do
+        ! allgather should be applied into the the r and vel array
 
-   endsubroutine vel_verlet
+    end subroutine vel_verlet
 
 !=====================================================================================!
 !                     VELOCITY VERLET INTEGRATION (With temperature)
 !=====================================================================================!
 !  Input:
 !      - natoms (número de átomos)(in) : integer scalar
-! 	   - r (positions)(inout) : double precision array
+!      - r (positions)(inout) : double precision array
 !      - vel (velocity)(inout) : double precision array
 !      - F (force)(inout) : double precision array
-! 	   -dt (time step)(in) : double precision scalar
-!	   -rc (cut-off distance) (in) : double precision
-!	   -boxlength (longitude of one side)(in) : double precision scalar
-!	   -gr ()(in) : double precision array
+!      -dt (time step)(in) : double precision scalar
+!      -rc (cut-off distance) (in) : double precision
+!      -boxlength (longitude of one side)(in) : double precision scalar
+!      -gr ()(in) : double precision array
 !      -deltag ()(in) : double precision scalar
 !  Output:
-!	   - r (positions)(inout) : double precision array
+!      - r (positions)(inout) : double precision array
 !      - vel (velocity)(inout) : double precision array
-!	   - Upot (potential energy)(out) : double precision scalar
-! 	   - temp (temperature)(in) : double precision scalar
-!	   - Pressp (pressure) (out) : double precision scalar
-!  Depencency:
+!      - Upot (potential energy)(out) : double precision scalar
+!      - temp (temperature)(in) : double precision scalar
+!      - Pressp (pressure) (out) : double precision scalar
+!  Dependencies:
 !      - force() : In module forces (src/modules/forces.f90)
 !      - andersen_thermo() : In module thermostat (src/modules/thermostats.f90)
 !=====================================================================================!
-	subroutine vel_verlet_with_thermo(natoms,r,vel,F,epot,dt,rc,boxlength,Temp,pressp,&
-					   gr,deltag,particle_range,sizes,displs,taskid)
-    	include "../declaration_variables/parallel_variables.h"
-		  integer,intent(in)::natoms, particle_range(2)
-    	integer, allocatable, intent(in):: sizes(:), displs(:)
-	   	double precision, allocatable,  intent(inout) :: F(:,:)
-	   	double precision, allocatable, intent(inout) :: r(:,:), vel(:,:)
-	   	double precision, allocatable,  intent(inout) :: gr(:)
-	   	double precision, intent(in) :: dt, rc, boxlength, Temp, deltag
-	   	double precision, intent(out) :: pressp, epot
-    	integer ii, jj, jv
+    subroutine vel_verlet_with_thermo(natoms, r, vel, F, epot, dt, rc, boxlength, Temp, pressp, &
+                                      gr, deltag, particle_range, sizes, displs, taskid)
+        include "../declaration_variables/parallel_variables.h"
+        integer, intent(in)::natoms, particle_range(2)
+        integer, allocatable, intent(in):: sizes(:), displs(:)
+        double precision, allocatable, intent(inout) :: F(:, :)
+        double precision, allocatable, intent(inout) :: r(:, :), vel(:, :)
+        double precision, allocatable, intent(inout) :: gr(:)
+        double precision, intent(in) :: dt, rc, boxlength, Temp, deltag
+        double precision, intent(out) :: pressp, epot
+        integer ii, jj, jv
 
-	    first_particle = particle_range(1); last_particle = particle_range(2)
-	    call MPI_BARRIER(MPI_COMM_WORLD, ierror)
-	    ! <------ aqui se necesitan las fuerzas repartidas entre todos los workers
-			do jj = particle_range(1),particle_range(2)
-	      jv = jj - particle_range(1) + 1
-				do ii = 1,3
-					r(jj,ii) = r(jj,ii) + vel(jv,ii)*dt + 0.5d0*F(jv,ii)*dt*dt
-					vel(jv,ii) = vel(jv,ii) + F(jv,ii)*0.5d0*dt
-				enddo
-			enddo
-	    	call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        first_particle = particle_range(1); last_particle = particle_range(2)
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        ! <------ aqui se necesitan las fuerzas repartidas entre todos los workers
+        do jj = particle_range(1), particle_range(2)
+            jv = jj - particle_range(1) + 1
+            do ii = 1, 3
+                r(jj, ii) = r(jj, ii) + vel(jv, ii)*dt + 0.5d0*F(jv, ii)*dt*dt
+                vel(jv, ii) = vel(jv, ii) + F(jv, ii)*0.5d0*dt
+            end do
+        end do
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
 
-		    do ii = 1,3
-		      call MPI_allgatherv(r(first_particle:last_particle,ii),(last_particle-first_particle+1),&
-				  MPI_DOUBLE_PRECISION,r(:,ii),sizes,displs,MPI_DOUBLE_PRECISION,&
-				  MPI_COMM_WORLD,ierror)
-		    end do
-		    call MPI_BARRIER(MPI_COMM_WORLD, ierror)
-		    ! allgather should be applied into the the r and vel array
-		    ! <------- aqui se necesita haber repartido todas las posiciones
-				call force(natoms,r,boxlength,rc,F,particle_range)
+        do ii = 1, 3
+            call MPI_allgatherv(r(first_particle:last_particle, ii), (last_particle - first_particle + 1), &
+                                MPI_DOUBLE_PRECISION, r(:, ii), sizes, displs, MPI_DOUBLE_PRECISION, &
+                                MPI_COMM_WORLD, ierror)
+        end do
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        ! allgather should be applied into the the r and vel array
+        ! <------- aqui se necesita haber repartido todas las posiciones
+        call force(natoms, r, boxlength, rc, F, particle_range)
 
-		    call potential(natoms,r,boxlength,rc,epot,pressp,gr,deltag,particle_range,taskid)
+        call potential(natoms, r, boxlength, rc, epot, pressp, gr, deltag, particle_range, taskid)
 
-		    call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
 
-				do jj = particle_range(1),particle_range(2)
-		      jv = jj - particle_range(1) + 1
-					do ii = 1,3
-			         vel(jv,ii) = vel(jv,ii) + F(jv,ii)*0.5d0*dt
-		      enddo
-		    enddo
+        do jj = particle_range(1), particle_range(2)
+            jv = jj - particle_range(1) + 1
+            do ii = 1, 3
+                vel(jv, ii) = vel(jv, ii) + F(jv, ii)*0.5d0*dt
+            end do
+        end do
 
-		    call andersen_thermo(Temp,vel,natoms,particle_range)
+        call andersen_thermo(Temp, vel, natoms, particle_range)
 
-		   endsubroutine vel_verlet_with_thermo
+    end subroutine vel_verlet_with_thermo
 
-endmodule integrators
+end module integrators
