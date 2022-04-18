9df61174eaff7402c330d3d975aed1f292c748e6
Pol Sanz <me@polsanz.xyz>
2022-04-18 20:25:09 +0200

    Output formatting

--- src/modules/boundary.f90
+++ src/modules/boundary.f90
@@ -1,50 +1,52 @@
 !==============================================================================!
 !                          MODULE BOUNDARY
 !==============================================================================!
-! Module that contains the procces to create periodic boundary conditions in 
+! Module that contains the procces to create periodic boundary conditions in
 ! a 3D squared system.
 !==============================================================================!
-! The module contains:
-!       -> pbc(): 
-!				Process that controls the periodic boundary conditions
-!                   Input:
-!                       - x (distance)(inout) : double precision scalar
-!								- boxlength (in) : double precision scalar
-!                       - origin (in) : double precision scalar
-!                   Output:
-!                       - x (distance)(inout) : double precision scalar
+! The module contains:														   !
+!       -> pbc():															   !
+!          Process that controls the periodic boundary conditions			   !
+!                   Input:													   !
+!                       - x (distance)(inout) : double precision scalar		   !
+!                       - boxlength (in) : double precision scalar			   !
+!                       - origin (in): double precision scalar				   !
+!                   Output:													   !
+!                       - x (distance)(inout) : double precision scalar        !
 !==============================================================================!
+
 module boundary
-	implicit none
- 
-contains   
+    implicit none
+
+contains
+
 !===========================================================================!
-!               				PERIODIC BOUNDARY CONDITIONS
+!                    PERIODIC BOUNDARY CONDITIONS							!
 !===========================================================================!
 ! Periodic boundary conditions function for forces.
 !===========================================================================!
-! Input:
-!		- x (distance)(inout) : double precision scalar
-!		- boxlength (in) : double precision scalar
-!		- origin (in) : double precision scalar
-!					Origin of the cell of simulation.
-! Output:
-!		- origin (in) : double precision scalar
-!					Origin of the cell of simulation.
+! Input:																	!
+!                - x (distance)(inout) : double precision scalar			!
+!                - boxlength (in) : double precision scalar					!
+!                - origin (in) : double precision scalar					!
+!                                        Origin of the cell of simulation.	!
+! Output:																	!
+!                - origin (in) : double precision scalar					!
+!                                        Origin of the cell of simulation.	!
 !===========================================================================!
-	subroutine pbc(x,boxlength,origin)
-		double precision, intent(in) :: boxlength,origin
-		double precision, intent(inout) :: x
 
-		if (x.gt.(origin + boxlength/2.d0)) then
-			x = x - boxlength
-			
-		elseif (x.lt.(origin - boxlength/2.d0)) then
-			x = x + boxlength
-			
-		endif
-		
-	endsubroutine pbc
+    subroutine pbc(x, boxlength, origin)
+        double precision, intent(in) :: boxlength, origin
+        double precision, intent(inout) :: x
 
+        if (x .gt. (origin + boxlength/2.d0)) then
+            x = x - boxlength
 
-endmodule boundary
+        elseif (x .lt. (origin - boxlength/2.d0)) then
+            x = x + boxlength
+
+        end if
+
+    end subroutine pbc
+
+end module boundary
